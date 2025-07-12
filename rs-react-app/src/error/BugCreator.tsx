95ee066c9dc248382cfb1fd9cd497d91916f2e9b
JulietBrn <shishkina.ja@gmail.com>
2025-07-12 19:58:24 +0200

    refactor: remove unneseccary code

--- rs-react-app/src/error/BugCreator.tsx
+++ rs-react-app/src/error/BugCreator.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 
 class BugCreator extends Component<unknown, { triggerError: boolean }> {
   constructor(props: unknown) {
