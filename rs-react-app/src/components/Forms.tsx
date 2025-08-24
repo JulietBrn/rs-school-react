54480350335b26bfc7dd69f54898169033958f5e
JulietBrn <shishkina.ja@gmail.com>
2025-08-24 12:18:19 +0200

    feat: implement zod

--- rs-react-app/src/components/Forms.tsx
+++ rs-react-app/src/components/Forms.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import Modal from './Modal';
 import { useForm } from 'react-hook-form';
 import { Gender } from './types';
+import { zodResolver } from '@hookform/resolvers/zod';
+import ErrorMessage from './ErrorMessage';
+import { schema } from '../schema';
 
 const INPUT_CLASS = 'border border-gray-300 p-2 rounded block mb-4';
 
@@ -11,12 +14,15 @@ export default function Forms() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
-  } = useForm();
+  } = useForm({ resolver: zodResolver(schema) });
 
-  // input control to upload picture (validate size and extension, allow png jpeg, save in redux store as base64)
+  // input control to upload picture (validate size and extension, save in redux store as base64)
   // autocomplete control to select country (all countries should be stored in the Redux store)
+
+  const onSubmit = (data: unknown) => {
+    console.log(data);
+  };
 
   return (
     <>
@@ -27,7 +33,7 @@ export default function Forms() {
       >
         Open modal
       </button>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         {/* Name */}
         <label htmlFor="name">Name</label>
         <input
@@ -35,17 +41,9 @@ export default function Forms() {
           id="name"
           className={INPUT_CLASS}
           autoComplete="name"
-          {...register('name', {
-            required: {
-              value: true,
-              message: 'Name is required',
-            },
-            pattern: {
-              value: /^[A-Z][a-zA-Z]*$/,
-              message: 'Name must start with an uppercase letter',
-            },
-          })}
+          {...register('name')}
         />
+        {errors.name?.message && <ErrorMessage message={errors.name.message} />}
 
         {/* Age */}
         <label htmlFor="age">Age</label>
@@ -53,17 +51,9 @@ export default function Forms() {
           type="number"
           id="age"
           className={INPUT_CLASS}
-          {...register('age', {
-            required: {
-              value: true,
-              message: 'Age is required',
-            },
-            min: {
-              value: 0,
-              message: 'Age must be a positive number',
-            },
-          })}
+          {...register('age')}
         />
+        {errors.age?.message && <ErrorMessage message={errors.age.message} />}
 
         {/* Email */}
         <label htmlFor="email">Email</label>
@@ -72,17 +62,11 @@ export default function Forms() {
           id="email"
           className={INPUT_CLASS}
           autoComplete="email"
-          {...register('email', {
-            required: {
-              value: true,
-              message: 'Email is required',
-            },
-            pattern: {
-              value: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
-              message: 'Email is not valid',
-            },
-          })}
+          {...register('email')}
         />
+        {errors.email?.message && (
+          <ErrorMessage message={errors.email.message} />
+        )}
 
         {/* Password */}
         <label htmlFor="password">Password</label>
@@ -90,30 +74,11 @@ export default function Forms() {
           type="password"
           id="password"
           className={INPUT_CLASS}
-          {...register('password', {
-            required: {
-              value: true,
-              message: 'Password is required',
-            },
-            minLength: {
-              value: 8,
-              message: 'Password must be at least 8 characters long',
-            },
-            validate: {
-              hasNumber: (value) =>
-                /[0-9]/.test(value) || 'Password must contain a number',
-              hasUppercase: (value) =>
-                /[A-Z]/.test(value) ||
-                'Password must contain an uppercase letter',
-              hasLowercase: (value) =>
-                /[a-z]/.test(value) ||
-                'Password must contain a lowercase letter',
-              hasSpecialChar: (value) =>
-                /[!@#$%^&*]/.test(value) ||
-                'Password must contain a special character',
-            },
-          })}
+          {...register('password')}
         />
+        {errors.password?.message && (
+          <ErrorMessage message={errors.password.message} />
+        )}
 
         {/* Password check */}
         <label htmlFor="confirmPassword">Confirm Password</label>
@@ -122,19 +87,11 @@ export default function Forms() {
           type="password"
           id="confirmPassword"
           className={INPUT_CLASS}
-          {...register('confirmPassword', {
-            required: {
-              value: true,
-              message: 'Confirm Password is required',
-            },
-            validate: {
-              matchesPreviousPassword: (value) => {
-                const { password } = watch();
-                return password === value || 'Passwords must match';
-              },
-            },
-          })}
+          {...register('confirmPassword')}
         />
+        {errors.confirmPassword?.message && (
+          <ErrorMessage message={errors.confirmPassword.message} />
+        )}
 
         {/* Gender */}
         <label htmlFor="gender">Gender</label>
@@ -143,6 +100,9 @@ export default function Forms() {
           <option value={Gender.male}>Male</option>
           <option value={Gender.other}>Other</option>
         </select>
+        {errors.gender?.message && (
+          <ErrorMessage message={errors.gender.message} />
+        )}
 
         {/* Checkbox */}
         <label className="block mb-4 ">
@@ -153,15 +113,23 @@ export default function Forms() {
           />
           I accept the terms and conditions
         </label>
+        {errors.terms?.message && (
+          <ErrorMessage message={errors.terms.message} />
+        )}
 
         {/* Picture upload */}
-        <label htmlFor="picture">Picture</label>
+        {/* <label htmlFor="picture">Picture</label>
         <input
           type="file"
           id="picture"
           className={INPUT_CLASS}
           {...register('picture')}
         />
+        {errors.picture?.message && (
+          <ErrorMessage message={errors.picture.message} />
+        )} */}
+
+        <button type="submit">Submit</button>
       </form>
       {isOpen && (
         <Modal onClose={() => setIsOpen(false)}>
